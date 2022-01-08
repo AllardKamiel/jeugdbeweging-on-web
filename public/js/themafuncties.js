69894159c196b0a5c50031362bd48851091e5fff
ArneDuyver <arne_duyver@hotmail.com>
2022-01-08 13:01:03 +0100

    Javascript bestanden geherorganiseerd

---
+++ public/js/themafuncties.js
@@ -0,0 +1,43 @@
+function zoekThema() {
+  var aantal = parseInt(document.getElementById("themazoeker_aantal").value);
+  axios.get("http://localhost:8088/api/themas/" + aantal, {
+    method: 'GET',
+    mode: 'no-cors'
+  })
+    .then((data) => {
+      var themas = data["data"];
+      var msg = '';
+      for (var i = 0; i < themas.length - 1; i++) {
+        msg += data["data"][i]["content"] + ", ";
+      }
+      msg += data["data"][themas.length - 1]["content"] + ".";
+      var id = "themazoeker_result";
+      setStatus(id, msg);
+      document.getElementById(id).style.color = "#fff";
+    },
+      (error) => {
+        console.log(error);
+      });
+}
+
+function opslaanThema() {
+  var thema_text = String(document.getElementById("themazoeker_text").value);
+  var id = "themazoeker_status";
+  axios.post("http://localhost:8088/api/themas", {
+    method: 'POST',
+    mode: 'no-cors',
+    data: {
+      'content': thema_text
+    },
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  })
+    .then((response) => {
+      console.log(response.status);
+      setStatus(id, response.status);
+    },
+      (error) => {
+        console.log(error);
+      })
+}
--- resources/views/materiaal.blade.php
+++ resources/views/materiaal.blade.php
@@ -13,5 +13,20 @@
   <p><strong>{{$materiaalnaam}}</strong></p>
   <button style="margin-top: 4px;" type="button" onclick="zoekMateriaal()">zoek</button>
   <p id="materiaalzoeker_result" style="color: red;margin-top: 4px;">Hier komt de info tevoorschijn!</p>
+  <div style="margin-top: 12px;">
+    <table id="materiaal_table" class="center">
+      <thead>
+          <tr>
+              <th>Materiaal</th>
+              <th>Locatie</th>
+              <th>Prijs per dag</th>
+              <th>Contactinfo</th>
+              <th>Aantal beschikbaar</th>
+              <th>Foto</th>
+          </tr>
+      </thead>
+    </table>
+  </div>
 </div>
+
 @stop
